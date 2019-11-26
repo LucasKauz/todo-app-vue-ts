e7ca7a9084c0e94b7680e8febd3ff83321d46753
Lucas Marques <lucaskauz@gmail.com>
2019-11-26 15:19:58 -0300

    feat: Store base

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,65 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export interface Task {
+  title: string,
+  description: string,
+  dueDate: string,
+  priority: string,
+  comment: string
+}
+
+interface TasksStore {
+  tasks: Task[]
+}
+
+export enum PriorityLevels {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high'
+}
+
+export const enum Types {
+  LIST_TASKS = 'LIST_TASKS',
+  LOAD_TASKS = 'LOAD_TASKS',
+  UPDATE_TASKS = 'UPDATE_TASKS',
+  ADD_TASK = 'ADD_TASK'
+}
+
+export default new Vuex.Store<TasksStore>({
   state: {
+    tasks: []
   },
   mutations: {
+    [Types.UPDATE_TASKS]: (state, payload: Task[]) => {
+      console.log('UPDATE_TASKS', payload)
+      state.tasks = [ ...payload ]
+    },
+    [Types.ADD_TASK]: (state, payload: Task) => {
+      console.log('ADD_TASK')
+      state.tasks = [ ...state.tasks, payload ]
+    }
   },
   actions: {
+    [Types.LOAD_TASKS] () {
+      return fetch('https://gist.githubusercontent.com/LucasKauz/43eefb1402b8578328199156254bef33/raw/0ca703a9b9f992aca578f395cc50d004bb0bc9dc/todo-list.json')
+        .then(response => response.json())
+    },
+    [Types.LIST_TASKS] ({ commit, dispatch }) {
+      dispatch(Types.LOAD_TASKS).then((response) => {
+        const { data = [] } = response
+        commit(Types.UPDATE_TASKS, data)
+      })
+    },
+    [Types.ADD_TASK] ({ commit }, payload) {
+      console.log(Types.ADD_TASK, payload)
+      fetch('https://gist.githubusercontent.com/LucasKauz/43eefb1402b8578328199156254bef33/raw/0ca703a9b9f992aca578f395cc50d004bb0bc9dc/todo-list.json')
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+          const task = jsonResponse.data[0]
+          commit(Types.ADD_TASK, task)
+        })
+    }
   },
   modules: {
   }
